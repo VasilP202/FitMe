062db26e84b879aa63e0a01e0bed7a43fc240725
Vasil Poposki <vasilpfc@gmail.com>
2022-10-18 20:49:47 +0200

    Basic client API

--- Backend/FitMe/clients/views.py
+++ Backend/FitMe/clients/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Client
+from .serializers import ClientSerializer
+
+
+@api_view(["GET", "POST"])
+def clients_list(request):
+    if request.method == "GET":
+        clients = Client.objects.all()
+        serializer = ClientSerializer(
+            clients, context={"request": request}, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = ClientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT", "DELETE"])
+def clients_detail(request, client_id):
+    try:
+        client = Client.objects.get(id=client_id)
+    except Client.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "PUT":
+        serializer = ClientSerializer(
+            client, data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "DELETE":
+        client.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
